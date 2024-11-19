8694a44e6d73be23c2e1288a3db31ad1832d28dc
Hassan Shahzad <hshahzad.bscs17seecs@seecs.edu.pk>
2024-11-20 01:11:26 +0500

    added rest files
    added testing files

---
+++ voice_check.py
@@ -0,0 +1,24 @@
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# Replace with your API key
+aai.settings.api_key = "9243b396ee824374a7244ede8701c438"
+
+# URL of the file to transcribe
+FILE_URL = "new.wav"
+
+# You can also transcribe a local file by passing in a file path
+# FILE_URL = './path/to/file.mp3'
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(FILE_URL)
+
+if transcript.status == aai.TranscriptStatus.error:
+    print(transcript.error)
+else:
+    print(transcript.text)
